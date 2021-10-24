9dea6b421dfa84dfa79453bf69fbeb1d0420e5f9
amir-al-razin <amir.alrazin@gmail.com>
2021-10-25 00:50:58 +0600

    oops

--- src/global-stores/useStore.ts
+++ src/global-stores/useStore.ts
@@ -2,16 +2,15 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 import { devtools } from "zustand/middleware";
 
-
 const sajdaVerseStore = (set) => ({
-  sajdaVerse:null,
-  addSajdaVerse:(verses)=>set({sajdaVerse:verses}),
+  sajdaVerse: null,
+  addSajdaVerse: (verses) => set({ sajdaVerse: verses }),
   removeAllBears: () => set({ bears: 0 }),
-})
+});
+const loadingStore = (set) => ({
+  loading: true,
+  setLoadingState: (state) => set({ loading: state }),
+});
 
-
-
-
-export const  useSajdaVerseStore = create(devtools(sajdaVerseStore));
-
-
+export const useSajdaVerseStore = create(devtools(sajdaVerseStore));
+export const useLoadingStore = create(devtools(loadingStore));
