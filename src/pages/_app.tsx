78864cfe944ba4fe332552625edc1368afb20da9
amir-al-razin <amir.alrazin@gmail.com>
2021-10-25 08:29:48 +0600

    feat: :sparkles: page-transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,33 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import Fonts from "../styles/fonts";
 import "@fontsource/poppins";
+import { AnimatePresence, motion } from "framer-motion";
 
 import theme from "../styles/theme";
 import Layout from "../components/layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
       <Layout>
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
       </Layout>
     </ChakraProvider>
   );
